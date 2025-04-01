50a0e5f8e4cc623e44ae8425f3ce2a94e7b441a3
SebastianDevps <guerrasebastian16@gmail.com>
2025-04-01 09:57:48 -0500

    agenda

---
+++ components/pages/home/agenda/components/Section1/section1.tsx
@@ -0,0 +1,9 @@
+export default function Section1() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center">
+        <h2 className="text-white text-4xl font-bold">Agenda parte 1</h2>
+      </div>
+    </div>
+  );
+}
---
+++ components/pages/home/agenda/components/Section2/section2.tsx
@@ -0,0 +1,9 @@
+export default function Section2() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center">
+        <h2 className="text-white text-4xl font-bold">Agenda parte 2</h2>
+      </div>
+    </div>
+  );
+}
