e5264e2d96a936f42c989027e1466a82da04dda5
SebastianDevps <guerrasebastian16@gmail.com>
2025-04-01 22:51:01 -0500

    fix: timeline ready

--- components/shared/header/DesktopHeader.tsx
+++ components/shared/header/DesktopHeader.tsx
@@ -6,7 +6,7 @@ import { DesktopHeaderProps } from "./type";
 
 export default function DesktopHeader({ navItems }: DesktopHeaderProps) {
   return (
-    <nav className="hidden lg:flex items-center gap-8">
+    <nav className="hidden lg:flex w-full items-center justify-end gap-8">
       {navItems.map((item) => (
         <Link
           key={item.name}
@@ -14,7 +14,7 @@ export default function DesktopHeader({ navItems }: DesktopHeaderProps) {
           className="text-md text-white/70 hover:text-[#00FF66] transition-colors relative group"
         >
           {item.name}
-          <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-[#00FF66] transition-all duration-300 group-hover:w-full"/>
+          <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-[#00FF66] transition-all duration-300 group-hover:w-full" />
         </Link>
       ))}
     </nav>
